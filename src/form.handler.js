108573a2730c705b424da316d1e83990cb565233
Dmitry Goman <DmitryGoman@coherentsolutions.com>
2019-06-20 18:02:31 +0300

    20.06.2019

---
+++ src/form.handler.js
@@ -0,0 +1,44 @@
+{/* <style>
+.error-message {
+    color: red;
+    display:none;
+}
+.error  input{
+    color: red;
+}
+.error-min .error-message-min {
+    display:block;
+}
+</style>
+
+min max
+<div class="form-group error error-min">
+<input id="asda" data-error-min="3" data-error-max="5" data-error-regexp="sdfsddd">
+<span class="error-message error-message-min">Error min length</span>
+<span class="error-message error-message-max">Error max length</span>
+</div>
+
+<script>
+
+var ErrorChecker = {
+    min: function (value, param) {
+        return value.length > param
+    }
+}
+
+function check(input) {
+    var inputValue 
+    errors = input -> data-error....
+    // errors = [{name: min, paraM: 3}, ]
+
+    for (let  i = 0;   < errors.length;  ++) {
+        
+        if (ErrorChecker[errors[i].name](inputValue, errors[i].paraM) ) {
+            addClass
+        }
+    }
+}
+
+
+
+</script> */}
