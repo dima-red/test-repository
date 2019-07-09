2cfebd799f7fe76bce29368389aca5a79423c156
Dmitry Goman <DmitryGoman@coherentsolutions.com>
2019-07-09 14:05:36 +0300

    ffff

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart/cart.service';
 
 @Component({
@@ -8,13 +9,25 @@ import { CartService } from '../cart/cart.service';
 })
 export class CartComponent implements OnInit {
     items: any;
+    checkoutForm: any;
     constructor(
-        private cartService: CartService
+        private cartService: CartService,
+        private formBuilder: FormBuilder
     ) {
-        this.items = this.cartService.getItems();
+        this.checkoutForm = this.formBuilder.group({
+            name: '',
+            address: ''
+        });
     }
 
     ngOnInit() {
+    }
+
+    onSubmit(customerData) {
+        console.warn('Your order has been submitted', customerData);
+
+        this.items = this.cartService.clearCart();
+        this.checkoutForm.reset();
     }
 
 }
