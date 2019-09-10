78db51c0f0a11edd533d0ce0af5092649ccc4a47
Dmitry Goman <DmitryGoman@coherentsolutions.com>
2019-09-10 11:26:25 +0300

    private snake

--- src/classes/game.js
+++ src/classes/game.js
@@ -3,133 +3,136 @@ import { Snake } from "./snake";
 import { Bot } from "./bot";
 import { GAME_SPEED, CANVAS_BORDER_COLOUR, CANVAS_BACKGROUND_COLOUR, FOODS } from "../constants/general-constants";
 
-export class Game {
-    appWrapper = document.querySelector(".app-wrapper");
-    canvas = null;
-    ctx = null;
-    foodInstances = [];
-    snakeInstances = [];
-    gameOverSnakesSet = new Set();
-    amountOfUsers = null;
-    numberOfUser = 0;
-    gameOverFlag = false;
+const appWrapper = document.querySelector(".app-wrapper");
+let canvas = null;
+let ctx = null;
+const foodInstances = [];
+const snakeInstances = [];
+const gameOverSnakesSet = new Set();
+let amountOfUsers = null;
+let numberOfUser = 0;
+let gameOverFlag = false;
+let amountOfBots = null;
+let onGameOver = null;
 
-    constructor(amountOfUsers, amountOfBots, onGameOver) {
-        this.onGameOver = onGameOver;
-        this.amountOfUsers = amountOfUsers;
-        this.amountOfBots = amountOfBots;
-        this.canvas = this.appWrapper.querySelector("#field");
-        this.ctx = this.canvas.getContext("2d");
-        this.createFoodInstances();
-        this.createSnakeInstances();
+const createFoodInstances = () => {
+    for (let i = 0; i < FOODS.length; i++) {
+        const foodNumber = i;
+        foodInstances.push(new Food(appWrapper, foodNumber));
+    }
+}
 
-        document.addEventListener("keydown", this.onChangeDirectionBtnClicked.bind(this));
-
-        this.main();
-        this.foodInstances.forEach(foodInstance => foodInstance.createFood(this.snakeInstances));
+const createSnakeInstances = () => {
+    for (let i = 0; i < amountOfUsers; i++) {
+        snakeInstances.push(new Snake(numberOfUser, appWrapper));
+        numberOfUser ++;
     }
 
-    createFoodInstances() {
-        for (let i = 0; i < FOODS.length; i++) {
-            const foodNumber = i;
-            this.foodInstances.push(new Food(this.appWrapper, foodNumber));
+    if (amountOfBots) {
+        for (let j = 0; j < amountOfBots; j++) {
+            snakeInstances.push(new Bot(numberOfUser, appWrapper));
+            numberOfUser ++;
         }
     }
 
-    createSnakeInstances() {
-        for (let i = 0; i < this.amountOfUsers; i++) {
-            this.snakeInstances.push(new Snake(this.numberOfUser, this.appWrapper));
-            this.numberOfUser ++;
-        }
+    console.info(snakeInstances);
+}
 
-        if (this.amountOfBots) {
-            for (let j = 0; j < this.amountOfBots; j++) {
-                this.snakeInstances.push(new Bot(this.numberOfUser, this.appWrapper));
-                this.numberOfUser ++;
-            }
-        }
-
-        console.info(this.snakeInstances);
+const main = () => {
+    if (gameOverFlag) {
+        onGameOver();
+        return;
     }
+    didGameEnd();
 
-    main() {
-        if (this.gameOverFlag) {
-            this.onGameOver();
-            return;
-        }
-        this.didGameEnd();
-
-        setTimeout(() => {
-            this.clearCanvas();
-            this.foodInstances.forEach(foodInstance => foodInstance.drawFood());
-            this.snakeInstances.forEach(snakeInstance => {
-                if (snakeInstance) {///////
-                    snakeInstance.resetChangingDirectionFlag();
-                    snakeInstance.advanceSnake(this.foodInstances);
-                    if (snakeInstance.botChangeDirection) {
-                        snakeInstance.botChangeDirection();
-                    }
-                    snakeInstance.drawSnake();
+    setTimeout(() => {
+        clearCanvas();
+        foodInstances.forEach(foodInstance => foodInstance.drawFood());
+        snakeInstances.forEach(snakeInstance => {
+            if (snakeInstance) {
+                snakeInstance.resetChangingDirectionFlag();
+                snakeInstance.advanceSnake(foodInstances);
+                if (snakeInstance.botChangeDirection) {
+                    snakeInstance.botChangeDirection();
                 }
-            });
-            this.main();
-            
-
-        }, GAME_SPEED);
-    }
-
-    clearCanvas() {
-        this.ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
-        this.ctx.strokestyle = CANVAS_BORDER_COLOUR;
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.strokeRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-
-    onChangeDirectionBtnClicked(ev) {
-        this.snakeInstances.forEach(snakeInstance => {
-            if (snakeInstance && snakeInstance.isItRealUser) {
-                snakeInstance.changeDirection(ev);
+                snakeInstance.drawSnake();
             }
         });
-    }
-
-    didGameEnd() {
-        this.snakeInstances.forEach(snakeInstance => {
-            const collisionWithYourself = snakeInstance && snakeInstance.checkCollisionWithYourself();
-            const collisionWithWall = snakeInstance && snakeInstance.checkCollisionWithWall();
-            const collisionWithOtherSnake = snakeInstance && snakeInstance.checkCollisionWithOtherSnakes(this.snakeInstances);
-
-            if (collisionWithOtherSnake >= 0 && collisionWithOtherSnake !== null) {
-                this.gameOverSnakesSet.add(collisionWithOtherSnake);
-            }
-
-            if (collisionWithYourself && collisionWithYourself.isGameOver) {
-                this.gameOverSnakesSet.add(collisionWithYourself.user);
-            };
-
-            if (collisionWithWall && collisionWithWall.isGameOver) {
-                this.gameOverSnakesSet.add(collisionWithWall.user);
-            }
-        });
+        main();
         
-        if (this.gameOverSnakesSet.size) {
-            this.gameOverSnakesSet.forEach(player => {
-                const gameOverSnake = this.snakeInstances.filter(snake => snake.numberOfUser === player);
-                
-                this.snakeInstances.splice(this.snakeInstances.indexOf(...gameOverSnake), 1);
-                this.showGameOver(player);
-            });
-            this.gameOverSnakesSet.clear();
 
-            if (!this.snakeInstances.length){
-                this.gameOverFlag = true;
-            }
+    }, GAME_SPEED);
+}
+
+const clearCanvas = () => {
+    ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
+    ctx.strokestyle = CANVAS_BORDER_COLOUR;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+}
+
+const onChangeDirectionBtnClicked = (ev) => {
+    snakeInstances.forEach(snakeInstance => {
+        if (snakeInstance && snakeInstance.isItRealUser) {
+            snakeInstance.changeDirection(ev);
+        }
+    });
+}
+
+const didGameEnd = () => {
+    snakeInstances.forEach(snakeInstance => {
+        const collisionWithYourself = snakeInstance && snakeInstance.checkCollisionWithYourself();
+        const collisionWithWall = snakeInstance && snakeInstance.checkCollisionWithWall();
+        const collisionWithOtherSnake = snakeInstance && snakeInstance.checkCollisionWithOtherSnakes(snakeInstances);
+
+        if (collisionWithOtherSnake >= 0 && collisionWithOtherSnake !== null) {
+            gameOverSnakesSet.add(collisionWithOtherSnake);
+        }
+
+        if (collisionWithYourself && collisionWithYourself.isGameOver) {
+            gameOverSnakesSet.add(collisionWithYourself.user);
+        };
+
+        if (collisionWithWall && collisionWithWall.isGameOver) {
+            gameOverSnakesSet.add(collisionWithWall.user);
+        }
+    });
+    
+    if (gameOverSnakesSet.size) {
+        gameOverSnakesSet.forEach(player => {
+            const gameOverSnake = snakeInstances.filter(snake => snake.numberOfUser === player);
+            
+            snakeInstances.splice(snakeInstances.indexOf(...gameOverSnake), 1);
+            showGameOver(player);
+        });
+        gameOverSnakesSet.clear();
+
+        if (!snakeInstances.length){
+            gameOverFlag = true;
         }
     }
+}
 
-    showGameOver(player) {
-        this.appWrapper
-            .querySelectorAll(".game-settings-wrapper .end-game")[player]
-            .classList.remove("hide");
+const showGameOver = (player) => {
+    appWrapper
+        .querySelectorAll(".game-settings-wrapper .end-game")[player]
+        .classList.remove("hide");
+}
+
+
+export class Game {
+    constructor(usersAmount, botsAmount, gameOverCB) {
+        onGameOver = gameOverCB;
+        amountOfUsers = usersAmount;
+        amountOfBots = botsAmount;
+        canvas = appWrapper.querySelector("#field");
+        ctx = canvas.getContext("2d");
+        createFoodInstances();
+        createSnakeInstances();
+
+        document.addEventListener("keydown", onChangeDirectionBtnClicked);
+
+        main();
+        foodInstances.forEach(foodInstance => foodInstance.createFood(snakeInstances));
     }
 }
