dd47dcd9b633ded8b92e6d5f104e5d1491db3cec
Dmitry Goman <DmitryGoman@coherentsolutions.com>
2019-09-11 20:16:02 +0300

    11.09.2019

--- src/components/form-seating.js
+++ src/components/form-seating.js
@@ -7,15 +7,16 @@ export class FormSeating {
     constructor() {
         this.appWrapper = document.querySelector(".app-wrapper");
         this.buildingSelectEl = this.appWrapper.querySelector(".building");
-        this.buildingSelectEl.addEventListener("change", this.onBuildingSelected.bind(this));
-        this.getBuildings();
         this.getDictionary();
-
-        
+        this.getBuildings();
+        this.buildingSelectEl.addEventListener("change", this.onBuildingSelected.bind(this));
+        document.addEventListener("mousedown", this.onMouseDown.bind(this));
+        document.addEventListener("mousemove", this.onMouseMove.bind(this));
+        document.addEventListener("mouseup", this.onMouseUp.bind(this));
     }
 
     getBuildings() {
-        fetch("https://lyceumexams.herokuapp.com/api/corpses")
+        fetch(process.env.BUILDINGS_URL)
             .then(response => response.json())
             .then(buildingsJson => {
                 this.buildings = buildingsJson
@@ -30,19 +31,21 @@ export class FormSeating {
 
             building.label = buildingData.name;
             building.value = buildingData.alias;
-            building.append(`${buildingData.name}`);
+            building.append(buildingData.name);
             this.buildingSelectEl.append(building);
         }
     }
 
     onBuildingSelected(ev) {
         const value = ev.target.selectedOptions[0].value;
+        const facultyName = ev.target.selectedOptions[0].label;
 
         this.getStudentsList(value);
+        this.renderSeatedStudents(facultyName);
     }
 
     getStudentsList(value) {
-        fetch(`https://lyceumexams.herokuapp.com/api/pupils?corps=${value}`)
+        fetch(`${process.env.STUDENTS_URL + value}`)
             .then(response => response.json())
             .then(responseJson => this.renderAllStudents(responseJson));
     }
@@ -51,11 +54,9 @@ export class FormSeating {
         console.log(allStudents);
         
         const tBodyEl = this.appWrapper.querySelector(".all-students tbody");
-
+        const allStudentsFragment = new DocumentFragment();
 
         for(const student of allStudents) {
-            console.log(student);
-
             const tr = document.createElement("tr");
             const tdNumber = document.createElement("td");
             const tdName = document.createElement("td");
@@ -65,16 +66,127 @@ export class FormSeating {
 
             tdNumber.append(allStudents.indexOf(student) + 1);
             tdName.append(student.firstName, ` ${student.lastName}`, ` ${student.parentName}`);
-            tdRoom.append();
-        }        
+            tdRoom.append(this.dictionary.audiences[student.audience]);
+            tdType.append(this.dictionary.profiles[student.profile]);
+
+            if (student.needBel) {
+                const div = document.createElement("div");
+                div.classList.add("flag");
+                tdNeedBel.append(div);
+            }
+
+            tr.append(tdNumber, tdName, tdRoom, tdType, tdNeedBel);
+            allStudentsFragment.append(tr);
+        }
+
+        tBodyEl.append(allStudentsFragment);
     }
 
-    renderSeatedStudents() {
+    onMouseDown(ev) {
+        if(ev.target.parentNode.tagName === "TR") {
+            // console.log();
+            const selectedRow = ev.target.parentNode;
+            this.fakeRow = document.createElement("div");
+            const fakeRowWrapper = document.createElement("div");
+            const fakeSpanNumber = document.createElement("span");
+            const fakeSpanName = document.createElement("span");
+            const fakeSpanRoom = document.createElement("span");
+            const fakeSpanType = document.createElement("span");
+            const fakeSpanBel = document.createElement("span");
+            // if() {
 
+            // }
+
+            fakeSpanNumber.append(4);
+            fakeSpanName.append("Иванов Петр Сергеевич");
+            fakeSpanRoom.append(234);
+            fakeSpanType.append("математический");
+            fakeSpanBel.append("флаг");
+
+            this.fakeRow.append(fakeSpanNumber, fakeSpanName, fakeSpanRoom, fakeSpanType, fakeSpanBel);
+            
+
+
+
+            // this.fakeRow.style.position = "absolute";
+            this.fakeRow.style.position = "background-color: white";
+            this.fakeRow.style.position = "border: 1px solid #212529";
+            // this.fakeRow.style.position = "display: inline-table";
+            
+            fakeRowWrapper.append(this.fakeRow);
+
+            this.moveAt(ev);
+            document.body.append(fakeRowWrapper);
+            fakeRowWrapper.style.zIndex = 1000;
+
+
+            console.log(fakeRowWrapper);
+        }
+    }
+
+    onMouseMove(ev) {
+        if(this.fakeRow) {
+            this.moveAt(ev);
+        }
+    }
+
+    onMouseUp() {
+        if(this.fakeRow) {
+            console.log("=========== End ===============");
+        }
+    }
+
+    moveAt(ev) {
+        // console.log(ev);
+        this.fakeRow.style.left = ev.pageX - this.fakeRow.offsetWidth / 2 + 'px';
+        this.fakeRow.style.top = ev.pageY - this.fakeRow.offsetHeight / 2 + 'px';
+    }
+
+    renderSeatedStudents(facultyName) {
+        const tBodyEl = this.appWrapper.querySelector(".seated-students tbody");
+        const seatedStudentsFragment = new DocumentFragment();
+        const facultyAudiences = [];
+
+        for(const faculty of this.buildings) {
+            if (faculty.name === facultyName) {
+                facultyAudiences.push(...faculty.places[0].audience);
+            }
+        }
+
+        console.log(facultyAudiences);
+
+        for (const audience of facultyAudiences) {
+            const tr = document.createElement("tr");
+            const tdNumber = document.createElement("td");
+            const tdAmount = document.createElement("td");
+            const tdMax = document.createElement("td");
+            const tdBel = document.createElement("td");
+
+            tdNumber.append(audience.name);
+            tdNumber.classList.add("bold");
+            tdAmount.append(audience.count);
+            tdMax.append(audience.max);
+
+            if (audience.bel) {
+                const div = document.createElement("div");
+                div.classList.add("flag");
+                tdBel.append(div);
+            }
+
+            tr.append(tdNumber, tdAmount, tdMax, tdBel);
+
+            if (audience.count > audience.max) {
+                tr.classList.add("table-danger");
+            }
+
+            seatedStudentsFragment.append(tr);
+        }
+
+        tBodyEl.append(seatedStudentsFragment);
     }
 
     getDictionary() {
-        fetch("https://lyceumexams.herokuapp.com/api/dictionary")
+        fetch(process.env.DICTIONARY_URL)
             .then(response => response.json())
             .then(dictionaryJson => {
                 this.dictionary = dictionaryJson
@@ -83,13 +195,14 @@ export class FormSeating {
     }
 
     getGeneratedStatus() {
-        fetch("https://lyceumexams.herokuapp.com/api/generateStatus")
+        fetch(process.env.GENERATE_STATUS_URL)
         .then(response => response.json())
         .then(statusJson => console.log(statusJson));
     }
 
-    // getData(url) {        
-    //     fetch(url)
+    // getData(url) { 
+        
+    //     return  fetch(url)
     //         .then(response => response.json())
     //         .then(responseJson => this.data = responseJson);
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.scss";
 import { FormSeating } from "./components/form-seating";
+// import "dotenv";
+require("dotenv").config(); // Why I can not import it?
 
 document.addEventListener('DOMContentLoaded', () => {
     new FormSeating();
