6c16e896d311e9e8af990ec2e9d5f3c6f9fefa76
Dmitry Goman <DmitryGoman@coherentsolutions.com>
2019-09-17 18:11:18 +0300

    17.09.2019

--- src/components/data-service.js
+++ src/components/data-service.js
@@ -2,44 +2,34 @@ export class DataService {
     dictionary = null;
     buildings = null;
 
-    constructor (renderBuildings, renderAllStudents) {
-        this.renderBuildings = renderBuildings;
+    constructor (renderBuildingsSelect, renderAllStudents) {
+        this.renderBuildingsSelect = renderBuildingsSelect;
         this.renderAllStudents = renderAllStudents;
         this.getDictionary();
         this.getBuildings();
     }
 
     getDictionary() {
-      
-            this.getData(process.env.DICTIONARY_URL)
-            .then(dictionaryJson => {
-                this.dictionary = dictionaryJson
-                console.info(this.dictionary);
-            })
-            .catch(err => console.error(err));
-        
+        this.getData(process.env.DICTIONARY_URL)
+        .then(dictionaryJson => {
+            this.dictionary = dictionaryJson
+            console.info(this.dictionary);
+        })
+        .catch(err => console.error(err));        
     }
 
     getBuildings() {
-        try {
-            this.getData(process.env.BUILDINGS_URL)
-            .then(buildingsJson => {
-                this.buildings = buildingsJson
-                console.info(this.buildings);
-                this.renderBuildings(this.buildings);
-            });
-        } catch (err) {
-            console.error(err);
-        }
+        this.getData(process.env.BUILDINGS_URL)
+        .then(buildingsJson => {
+            this.buildings = buildingsJson
+            console.info(this.buildings);
+            this.renderBuildingsSelect(this.buildings);
+        })
+        .catch(err => console.error(err));
     }
 
     getStudentsList(value) {
-        try {
-            this.getData(process.env.STUDENTS_URL + value)
-            .then(responseJson => this.renderAllStudents(responseJson));
-        } catch (err) {
-            console.error(err);
-        }
+        return this.getData(process.env.STUDENTS_URL + value);
     }
 
     getData(url) { 
--- src/components/form-seating.js
+++ src/components/form-seating.js
@@ -5,18 +5,27 @@ export class FormSeating {
     dataServiceInstance = null;
     buildingSelectEl = null; 
     appWrapper = null;
+    fakeRow = null;
+    allStudentsTableWidth = null;
+    facultyAudiences = null;
+    facultyName = null;
+    toTheTop = 202;
+    tableHeight = null;
+    tableY = null;
 
     constructor() {
         this.appWrapper = document.querySelector(".app-wrapper");
         this.buildingSelectEl = this.appWrapper.querySelector(".building");
-        this.dataServiceInstance = new DataService(this.renderBuildings, this.renderAllStudents);
+        this.dataServiceInstance = new DataService(this.renderBuildingsSelect, this.renderAllStudents);
         this.buildingSelectEl.addEventListener("change", this.onBuildingSelected.bind(this));
+        this.buildingSelectEl.addEventListener("change", this.onProfileSelected.bind(this));
+        this.buildingSelectEl.addEventListener("change", this.onAudienceSelected.bind(this));
         document.addEventListener("mousedown", this.onMouseDown.bind(this));
         document.addEventListener("mousemove", this.onMouseMove.bind(this));
         document.addEventListener("mouseup", this.onMouseUp.bind(this));
     }
 
-    renderBuildings = (buildings) => {
+    renderBuildingsSelect = (buildings) => {
         for(const buildingData of buildings) {
             const building = document.createElement("option");
 
@@ -27,22 +36,50 @@ export class FormSeating {
         }
     }
 
+    renderProfilesSelect() {
+
+    }
+
+    renderAudiencesSelect() {
+
+    }
+
     onBuildingSelected(ev) {
         const value = ev.target.selectedOptions[0].value;
-        const facultyName = ev.target.selectedOptions[0].label;
+        this.facultyName = ev.target.selectedOptions[0].label;
 
-        this.dataServiceInstance.getStudentsList(value);
-        this.renderSeatedStudents(facultyName);
+        this.dataServiceInstance.getStudentsList(value)
+            .then(responseJson => {
+                // console.info(responseJson);
+                this.renderAllStudents(responseJson);
+            })
+            .catch(err => console.error(err));
+        this.renderSeatedStudents();
+        this.tableHeight = document.querySelectorAll(".seated-students table")[0].clientHeight - 37;
+        this.amountOfRows = this.facultyAudiences.length;
+        this.rowHeight = this.tableHeight / this.amountOfRows;
+        this.tableY = this.tableHeight + this.toTheTop;
+    }
+
+    onProfileSelected() {
+
+    }
+
+    onAudienceSelected() {
+
     }
 
     renderAllStudents = (allStudents) => {
-        console.info(allStudents);
+        if (allStudents) {
+            this.allStudents = allStudents;
+        }
+        
         const tBodyEl = this.appWrapper.querySelector(".all-students tbody");
         const allStudentsFragment = new DocumentFragment();
 
         this.clearTableBody(tBodyEl);
 
-        for(const student of allStudents) {
+        for (const student of this.allStudents) {
             const tr = document.createElement("tr");
             const tdNumber = document.createElement("td");
             const tdName = document.createElement("td");
@@ -50,7 +87,7 @@ export class FormSeating {
             const tdType = document.createElement("td");
             const tdNeedBel = document.createElement("td");
 
-            tdNumber.append(allStudents.indexOf(student) + 1);
+            tdNumber.append(this.allStudents.indexOf(student) + 1);
             tdName.append(student.firstName, ` ${student.lastName}`, ` ${student.parentName}`);
             tdRoom.append(this.dataServiceInstance.dictionary.audiences[student.audience]);
             tdType.append(this.dataServiceInstance.dictionary.profiles[student.profile]);
@@ -70,94 +107,7 @@ export class FormSeating {
         tBodyEl.append(allStudentsFragment);
     }
 
-    onMouseDown(ev) {
-        if(ev.target.parentNode.className === "row-hower all-students") {
-            this.allStudentsTableWidth = ev.target.parentNode.clientWidth;
-            this.numberOfDraggedRow = parseInt(ev.target.parentNode.children[0].innerHTML);
-            this.fakeRow = document.createElement("div");
-            this.fakeRow.className = "draggableRow";
-            const fakeDivNumber = document.createElement("div");
-            const fakeDivName = document.createElement("div");
-            const fakeDivRoom = document.createElement("div");
-            const fakeDivType = document.createElement("div");
-            const fakeDivBel = document.createElement("div");
-            const personInfo = ev.target.parentNode.children;
-
-            if(ev.target.parentNode.lastChild.children[0]) {
-                const div = document.createElement("div");
-                div.classList.add("flag");
-                fakeDivBel.append(div);;
-            }
-
-            fakeDivNumber.append(personInfo[0].innerText);
-            fakeDivName.append(personInfo[1].innerText);
-            fakeDivRoom.append(personInfo[2].innerText);
-            fakeDivType.append(personInfo[3].innerText);
-
-            this.fakeRow.append(fakeDivNumber, fakeDivName, fakeDivRoom, fakeDivType, fakeDivBel);
-            this.fakeRow.classList.add("fake-row");
-            Array.from(this.fakeRow.children).forEach(child => child.classList.add("fake-row-item"));
-            document.body.append(this.fakeRow);
-            this.moveAt(ev);
-        }
-    }
-
-    onMouseMove(ev) {
-        if(this.fakeRow) {
-            this.moveAt(ev);
-        }
-    }
-
-    moveAt(ev) {
-        this.fakeRow.style.left = ev.pageX - this.fakeRow.offsetWidth / 2 + 'px';
-        this.fakeRow.style.top = ev.pageY - this.fakeRow.offsetHeight / 2 + 'px';
-    }
-
-    onMouseUp(ev) {
-        if(this.fakeRow) {            
-            const allDraggedRows = document.getElementsByClassName("draggableRow");
-            const theLastDraggedRow = allDraggedRows[allDraggedRows.length - 1];
-            const mouseX = ev.x;
-            const mouseY = ev.y;
-            const toTheTop = 202;
-            const tableHeight = document.querySelectorAll(".seated-students table")[0].clientHeight - 37;
-            const amountOfRows = this.facultyAudiences.length;
-            const rowHeight = tableHeight / amountOfRows;
-            const tableY = tableHeight + toTheTop;
-            let numberOfTheRow = amountOfRows;
-
-            for (let i = tableY; i >= toTheTop; i -= rowHeight) {
-                if (mouseY <= i && mouseY > (i - rowHeight) && mouseY > toTheTop && mouseX > this.allStudentsTableWidth) {
-                    const targetAudience = this.facultyAudiences[numberOfTheRow - 1];
-                    const newAmountOfStudents = ++ targetAudience.count;
-                    const updatedSeatedStudentsRow = document.querySelectorAll(".seated-students tr")[numberOfTheRow];
-                    const maxNumberOfStudents = targetAudience.max;
-                    const selectedRow = document.querySelectorAll(".all-students tr")[this.numberOfDraggedRow];
-
-                    if (selectedRow.children[2].innerHTML !== targetAudience.name) {
-                        updatedSeatedStudentsRow.children[1].innerHTML = newAmountOfStudents;
-                        selectedRow.children[2].innerHTML = targetAudience.name;
-                    }
-
-                    if (newAmountOfStudents > maxNumberOfStudents) {
-                        updatedSeatedStudentsRow.classList.add("table-danger");
-                        updatedSeatedStudentsRow.classList.add("row-hower-danger");
-                    }
-                    
-
-                } else if (numberOfTheRow >= 1 && mouseY > toTheTop) {
-                    numberOfTheRow--;
-                } else {
-                    console.info("outside of the seated students");
-                }
-            }
-
-            theLastDraggedRow.innerHTML = "";
-            theLastDraggedRow.classList.remove("fake-row");
-        }
-    }
-
-    renderSeatedStudents(facultyName) {
+    renderSeatedStudents() {
         const tBodyEl = this.appWrapper.querySelector(".seated-students tbody");
         const seatedStudentsFragment = new DocumentFragment();
         this.facultyAudiences = [];
@@ -165,12 +115,10 @@ export class FormSeating {
         this.clearTableBody(tBodyEl);
 
         for(const faculty of this.dataServiceInstance.buildings) {
-            if (faculty.name === facultyName) {
+            if (faculty.name === this.facultyName) {
                 faculty.places.forEach(place => this.facultyAudiences.push(...place.audience));
             }
         }
-
-        console.info(this.facultyAudiences);
 
         for (const audience of this.facultyAudiences) {
             const tr = document.createElement("tr");
@@ -202,6 +150,93 @@ export class FormSeating {
         }
 
         tBodyEl.append(seatedStudentsFragment);
+    }
+
+    onMouseDown(ev) {
+        if(ev.target.parentNode.className === "row-hower all-students") {
+            this.allStudentsTableWidth = ev.target.parentNode.clientWidth;
+            this.numberOfDraggedRow = parseInt(ev.target.parentNode.children[0].innerHTML);
+            this.fakeRow = document.createElement("div");
+            this.fakeRow.className = "draggableRow";
+            const fakeDivNumber = document.createElement("div");
+            const fakeDivName = document.createElement("div");
+            const fakeDivRoom = document.createElement("div");
+            const fakeDivType = document.createElement("div");
+            const fakeDivBel = document.createElement("div");
+            const personInfo = ev.target.parentNode.children;
+            const isFlag = ev.target.parentNode.lastChild.children[0];
+
+            if (isFlag) {
+                const div = document.createElement("div");
+                div.classList.add("flag");
+                fakeDivBel.append(div);;
+            }
+
+            fakeDivNumber.append(personInfo[0].innerText);
+            fakeDivName.append(personInfo[1].innerText);
+            fakeDivRoom.append(personInfo[2].innerText);
+            fakeDivType.append(personInfo[3].innerText);
+
+            this.fakeRow.append(fakeDivNumber, fakeDivName, fakeDivRoom, fakeDivType, fakeDivBel);
+            this.fakeRow.classList.add("fake-row");
+            Array.from(this.fakeRow.children).forEach(child => child.classList.add("fake-row-item"));
+            document.body.append(this.fakeRow);
+            this.moveAt(ev);
+        }
+    }
+
+    onMouseMove(ev) {
+        if(this.fakeRow) {
+            this.moveAt(ev);
+        }
+    }
+
+    moveAt(ev) {
+        this.fakeRow.style.left = ev.pageX - this.fakeRow.offsetWidth / 2 + 'px';
+        this.fakeRow.style.top = ev.pageY - this.fakeRow.offsetHeight / 2 + 'px';
+    }
+
+    onMouseUp(ev) {
+        if(this.fakeRow) {
+            const allDraggedRows = document.getElementsByClassName("draggableRow");
+            const theLastDraggedRow = allDraggedRows[allDraggedRows.length - 1];
+            const mouseX = ev.x;
+            const mouseY = ev.y;
+
+            if (mouseY <= this.tableY && mouseY > this.toTheTop && mouseX > this.allStudentsTableWidth) {
+                const numberOfDroppedRow = parseInt((mouseY - this.toTheTop) / this.rowHeight) + 1;                
+                const targetAudience = this.facultyAudiences[numberOfDroppedRow - 1];
+                const previousAllStudentsAudience = this.allStudents[this.numberOfDraggedRow - 1].audience;
+
+                if (this.checkBel(targetAudience, this.allStudents[this.numberOfDraggedRow - 1])) {
+                    alert("Попытка посадить белоруса к не белорусам!");
+
+                    this.removeFakeRow(theLastDraggedRow);
+
+                } else if (targetAudience._id !== this.allStudents[this.numberOfDraggedRow - 1].audience) {
+                    const previousSeatedStudentsAudience = this.facultyAudiences.filter(el => el._id === previousAllStudentsAudience);
+
+                    previousSeatedStudentsAudience[0].count --;
+                    targetAudience.count ++;
+                    this.allStudents[this.numberOfDraggedRow - 1].audience = targetAudience._id
+                    this.renderSeatedStudents();
+                    this.renderAllStudents();
+                }
+
+                this.fakeRow = null;
+            }
+
+            this.removeFakeRow(theLastDraggedRow);
+        }
+    }
+
+    checkBel(newAudience, oldAudience) {
+        return newAudience.bel !== oldAudience.needBel;
+    }
+
+    removeFakeRow(row) {
+        row.innerHTML = "";
+        row.classList.remove("fake-row");
     }
 
     getGeneratedStatus() {
