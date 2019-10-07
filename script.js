d0a2799b6debc83e90b30f82b08fdf80333b9386
Dmitry Goman <DmitryGoman@coherentsolutions.com>
2019-10-07 19:43:52 +0300

    07.10.2019

--- script.js
+++ script.js
@@ -4,31 +4,38 @@ angular
     .controller("ScopeController2", ScopeController2)
     .controller("ThisController1", ThisController1)
     .controller("ThisController2", ThisController2)
+    .service("MyService", MyService)
 
-function ScopeController1($scope) {
-    $scope.name = "Egor";
+function ScopeController1($scope, MyService) {
+    $scope.MyService = MyService;
     $scope.logThisAndScope = function() {
         console.log(this, $scope);
     }
 }
 
-function ScopeController2($scope) {
-    // $scope.name = "Vasya";
+function ScopeController2($scope, MyService) {
+    $scope.MyService = MyService;
     $scope.logThisAndScope = function() {
         console.log(this, $scope);
     }
 }
 
-function ThisController1($scope) {
+function ThisController1($scope, MyService) {
+    this.MyService = MyService;
     this.name = "Egor";
     this.logThisAndScope = function() {
         console.log(this, $scope);
     }
 }
 
-function ThisController2($scope) {
+function ThisController2($scope, MyService) {
+    this.MyService = MyService;
     // this.name = "Vasya";
     this.logThisAndScope = function() {
         console.log(this, $scope);
     }
+}
+
+function MyService() {
+    this.name = "Vasya";
 }
